530f9f8a54b4be8f7b10d9ee58d01a461dc742f6
en4395 <59679379+en4395@users.noreply.github.com>
2023-03-01 13:17:57 -0500

    Updated Driver

--- privateLibrary/src/privateLibrary/Driver.java
+++ privateLibrary/src/privateLibrary/Driver.java
@@ -7,27 +7,16 @@ import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
-		DueDate date=new DueDate();
-		Scanner sc=new Scanner(System.in);
 		
-		System.out.println(date.getDue());
 		Book book1=new Book("On Beauty", "Smith, Zadie", "0-141-01945-X");
-		book1.print();
 		
-		
-		User user1=new User("Ella Noyes");
-		User user2=new User("Tom Noyes");
-		User user3=new User("Ellie Couture");
+		User user1=new User("Zed Amoura");
+		User user2=new User("Vincent Alaya");
+		User user3=new User("Traci Carlyle");
 		
 		user1.borrow(book1);
 		// TODO Auto-generated method stub
-
-		LinkedList<User> myList=new LinkedList();
-		myList.addNode(user1);
-		myList.addNode(user2);
-		myList.addNode(user3);
-		myList.print();
-		
+	
 		System.out.print("\n"+book1);
 		
 		Library concordia=new Library();
